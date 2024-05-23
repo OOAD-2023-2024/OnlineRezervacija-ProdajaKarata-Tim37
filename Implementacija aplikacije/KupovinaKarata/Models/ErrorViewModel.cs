77604b5232df19343aabda21b4817593ec33b80b
Denan02 <dpoturak1@etf.unsa.ba>
2024-05-23 13:28:38 +0200

    Migracija baze

--- Implementacija aplikacije/KupovinaKarata/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KupovinaKarata.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Implementacija aplikacije/KupovinaKarata/Models/KategorijaManifestacije.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace KupovinaKarata.Models
-{
-    public enum KategorijaManifestacije
-    {
-    }
-}
